4529b62c7e7bf051064d24943a68434b2d7786eb
Leviathan <ojoaosoaressantos@gmail.com>
2024-03-05 14:31:05 -0300

    Fix.

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post("/register", (req, res) => {
                 }, 
 
                 tls : {
-                    rejectUnauthorized: true
+                    rejectUnAuthorized: true
                 }
             });
 
