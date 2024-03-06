07a10477b7a0f9ca88af2d60eb772fab0af6b987
Leviathan <132393418+ojoaosoares@users.noreply.github.com>
2024-03-06 02:24:52 -0300

    Delete join_us.session.sql

--- join_us.session.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    constraint_name,
-    table_name,
-FROM
-    information_schema.table_constraints
-WHERE
-    table_schema = 'railway' AND
-    table_name = 'users' AND
-    constraint_type = 'CHECK';
